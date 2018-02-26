b2b18115a0d9463f288c34be0405b4f09c03abeb
home <home@10.84.24.51>
2018-02-26 16:44:57 -0500

    Added methods to see if a clue is legal & which team won+needed clases

---
+++ src/code/Card.java
@@ -0,0 +1,16 @@
+package code;
+
+public class Card {
+	
+	String type;
+	
+	public Card(String cardType) {
+		type = cardType;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+
+}
--- src/code/code.java
+++ src/code/code.java
@@ -2,4 +2,30 @@ package code;
 
 public class code {
 
+	String codeName;
+	
+	
+	public boolean legalClue(Clue clue) {
+		if(clue.getClue()==codeName&&clue.cardRevealed==false) {
+			return false;
+		}	
+		return true;	
+	}
+	
+	
+	public Team winningTeam(Team team1, Team team2) {
+		if(team1.AssassinRevealed()==true) {
+			return team2;
+		}
+		else if(team2.AssassinRevealed()==true){
+			return team1;
+		}
+		else {
+			System.out.println("There is no winning team currently.");
+			return null;
+		}
+		
+		
+	}
+	
 }
