6be5ac857980c1653bbfc501cd2ae2e492513538
alexc <alexc@LAPTOP-14GEF48K>
2018-02-26 15:35:01 -0500

    updates to board

--- src/code/Board.java
+++ src/code/Board.java
@@ -6,7 +6,7 @@ public class Board {
 		String[][] board = new String[5][5];
 		for(int row = 0; row < board.length; row++) {
 			for(int col = 0; col < board[row].length; col++) {
-				Person p = new Person(identity);
+				Person p = new Person(identity); //I need a way to have a new person identity per space like the game has
 				board[row][col] = p.getIdentity();
 				
 			}
