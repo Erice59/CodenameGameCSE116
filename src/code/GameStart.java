1eafe721daa42caebc2fa1a3665a543aebfb721e
Erice59 <erice59@users.noreply.github.com>
2018-02-27 22:31:48 -0500

    Added GameStart class and test cases.

---
+++ src/code/GameStart.java
@@ -0,0 +1,68 @@
+package code;
+/**
+ * Class creates the board object that contains the 25 location instances and ensures each is assigned a codename, person, and is not revealed.
+ * Also sets the red team to have the first turn.
+ * @author Eric Weinman
+ *
+ */
+public class GameStart {
+
+	//private boolean teamTurn = true;
+	/**
+	 * This instance variable contains the information for which team's turn it currently is.
+	 */
+	private String currentTeamMove = "";
+	/**
+	 * Instance variable containing the board to be used during the game. The board is composed of an array of 25 location objects.
+	 */
+	private Board gameBoard;
+	/**
+	 * Default constructor that creates an empty board composed of 25 location objects without proper information and sets the first team move to red.
+	 */
+	public GameStart() {
+		setGameBoard(new Board());
+		setCurrentTeamMove("Red");
+	}
+	/**
+	 * Full constructor that creates a board of the given size and populates each location with a codename, person, and is not revealed.
+	 * Also sets red to be the first team to go.
+	 * @param size The size of the board to create (example given 5 it will create a 5 by 5 location array holding 25 locations).
+	 * @param filePath Path to the file containing the codenames to use.
+	 */
+	public GameStart(int size, String filePath) {
+		Board board = new Board(size, filePath);
+		setGameBoard(board);
+		setCurrentTeamMove("Red");
+	}
+	/**
+	 * Method to return which team currently is taking their turn.
+	 * @return A string designating which team is currently taking their turn, either Blue or Red.
+	 */
+	public String getCurrentTeamMove() {
+		return currentTeamMove;
+	}
+	/**
+	 * Method to set which team is currently taking their turn.
+	 * @param currentTeamMove A string to designate which team is currently taking their turn, either Blue or Red.
+	 */
+	public void setCurrentTeamMove(String currentTeamMove) {
+		this.currentTeamMove = currentTeamMove;
+	}
+	/**
+	 * Method to return the current board being used for the game.
+	 * @return Board which is an array of locations holding codenames, persons, and revealed status.
+	 */
+	public Board getGameBoard() {
+		return gameBoard;
+	}
+	/**
+	 * Method to set the current board to a different one.
+	 * @param gameBoard A board which is an array of location objects.
+	 */
+	public void setGameBoard(Board gameBoard) {
+		this.gameBoard = gameBoard;
+	}
+	
+	
+	
+}
---
+++ src/tests/GameStartTest.java
@@ -0,0 +1,51 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.Test;
+
+import code.Board;
+import code.GameStart;
+import code.Location;
+
+public class GameStartTest {
+
+	@Test
+	public void testDefaultConstructorTeamMove() {
+		GameStart test = new GameStart();
+		assertEquals("Red", test.getCurrentTeamMove());
+	}
+	
+	@Test
+	public void testFullConstructor() {
+		GameStart test = new GameStart(5, "Data/25Words.txt");
+		Board testBoard = new Board(5, "Data/25Words.txt");
+		Location[][] fromBoard = testBoard.getBoard();
+		Location[][] fromGame = test.getGameBoard().getBoard();
+		List<String> boardCodenames = new ArrayList<String>();
+		List<String> gameCodenames = new ArrayList<String>();
+		List<String> boardPersons = new ArrayList<String>();
+		List<String> gamePersons = new ArrayList<String>();
+		for (int row = 0; row < fromBoard.length; row++) {
+			for (int col = 0; col < fromBoard[row].length; col++) {
+				boardCodenames.add(fromBoard[row][col].get_codename());
+				boardPersons.add(fromBoard[row][col].get_person());
+			}
+		}
+		for (int row = 0; row < fromGame.length; row++) {
+			for (int col = 0; col < fromGame[row].length; col++) {
+				gameCodenames.add(fromGame[row][col].get_codename());
+				gamePersons.add(fromGame[row][col].get_person());
+			}
+		}
+		Collections.sort(boardCodenames);
+		Collections.sort(boardPersons);
+		Collections.sort(gameCodenames);
+		Collections.sort(gamePersons);
+		assertEquals(boardCodenames, gameCodenames);
+		assertEquals(boardPersons, boardPersons);
+		assertEquals("Red", test.getCurrentTeamMove());
+	}
+}
