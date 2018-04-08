277322957541f830e63f4360e6fdf945f943f5ed
JordanClem <31521143+JordanClem@users.noreply.github.com>
2018-04-08 17:31:43 -0400

    Updates to spymasterturn

--- src/code/gui/SpymasterTurn.java
+++ src/code/gui/SpymasterTurn.java
@@ -1,4 +1,34 @@
 package code.gui;
+import javax.swing.JOptionPane;
+
+import code.GameStart;
 
 public class SpymasterTurn {
+	
+	private static GameStart gs;
+	
+	public SpymasterTurn(GameStart gs){
+		this.gs = gs;
+	}
+	
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "It's" + gs.getCurrentTeamMove() + "ok");
+		countEnter();
+		clueEnter();
+	}
+	
+	
+	
+	//still make sure the counts legal
+	public static void countEnter(){
+		int count = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter the count!"));
+		
+		
+	}
+	
+	//still make sure clue is legal
+	public static void clueEnter(){
+		String clue = JOptionPane.showInputDialog(null,"Enter the clue!");
+	}
+
 }
