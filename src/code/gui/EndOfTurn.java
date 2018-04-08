2ed26b00440dd24611af0196bd5e4140fdc31386
alexc_mkqazvp <alexc_mkqazvp@DESKTOP-BH60AK6>
2018-04-08 16:43:49 -0400

    # WARNING: head commit changed in the meantime
    New classes for the spymaster turn, other for rest of team

---
+++ src/code/gui/EndOfTurn.java
@@ -0,0 +1,8 @@
+package code.gui;
+
+public class EndOfTurn {
+
+	public void EndTurn(WinningState w) {
+		
+	}
+}
---
+++ src/code/gui/TeamTurn.java
@@ -0,0 +1,36 @@
+package code.gui;
+
+import javax.swing.JFrame;
+
+import code.Clue;
+
+public class TeamTurn {
+	
+	/**
+	 * method that sets count and clue from spymaster
+	 */
+	public void showCountAndClue() {
+		
+	}
+	
+	/**
+	 * revealed loctions only display Blue Agent/Red Agent/Assassin/Innocent Bystander assigned to them
+	 */
+	public void revealed() {
+		
+	}
+	
+	/**
+	 * ends turn
+	 */
+	public void endTurn() {
+		
+	}
+	
+	/**
+	 * revealed Locations cannot be selected
+	 */
+	public void noSelectReveled() {
+		
+	}
+}
