c77cd84f96bb9775c682c844d06483919c741e81
Erice59 <Erice59@users.noreply.github.com>
2018-04-15 17:34:40 -0400

    Debug Utilities and EE related Changes

--- src/code/gui/MenuCreation.java
+++ src/code/gui/MenuCreation.java
@@ -15,34 +15,53 @@ public class MenuCreation {
 		start.setName("start");
 		start.addActionListener(new menuItemListener());
 		fileMenu.add(start);
-		
+
 		JMenuItem exit = new JMenuItem("Quit Application");
 		exit.setName("exit");
 		exit.addActionListener(new menuItemListener());
 		fileMenu.add(exit);
 		return fileMenu;
 	}
-	
+
 	public static JMenu helpMenuCreator() {
 		JMenu helpMenu = new JMenu("Help");
-		
+
 		JMenuItem about = new JMenuItem("About");
 		about.setName("about");
 		about.addActionListener(new menuItemListener());
 		helpMenu.add(about);
-		
-		JMenuItem debug = new JMenuItem("Debug");
-		debug.setName("debug");
+
+		JMenuItem debug = new JMenuItem("Change Board View");
+		debug.setName("boardView");
 		debug.addActionListener(new menuItemListener());
 		helpMenu.add(debug);
-		MainWindow.get_window().revalidate();
-		MainWindow.get_window().setVisible(false);
-		MainWindow.get_window().setVisible(true);
+		//MainWindow.get_window().revalidate();
+		//MainWindow.get_window().setVisible(false);
+		//MainWindow.get_window().setVisible(true);
+
+		JMenuItem teamView = new JMenuItem("Team Board View");
+		teamView.setName("teamView");
+		teamView.addActionListener(new menuItemListener());
+		helpMenu.add(teamView);
 		
+		JMenuItem spyView = new JMenuItem("Spymaster Board View");
+		teamView.setName("spyView");
+		spyView.addActionListener(new menuItemListener());
+		helpMenu.add(spyView);
+
+
+		JMenuItem cheatView = new JMenuItem("Board Cheat View");
+		cheatView.setName("cheatView");
+		cheatView.addActionListener(new menuItemListener());
+		helpMenu.add(cheatView);
+		//MainWindow.get_window().revalidate();
+		//MainWindow.get_window().setVisible(false);
+		//MainWindow.get_window().setVisible(true);
+
 		return helpMenu;
 	}
-	
-	
+
+
 	private static class menuItemListener implements ActionListener {
 
 		@Override
@@ -52,32 +71,44 @@ public class MenuCreation {
 				System.out.println("start");
 				MainWindow.initGameBoard();
 			}
-			
 			else if (jmi.getName().equals("exit")) {
 				System.out.println("exit");
 				MainWindow.get_window().setVisible(false);
 				MainWindow.get_window().dispose();
 			}
-			
 			else if (jmi.getName().equals("about")) {
 				System.out.println("about");
 				JOptionPane.showMessageDialog(null, "Easter EGG TBD");
+				About.aboutTeam();
 			}
-			
 			else if (jmi.getName().equals("guide")) {
 				
 			}
-			
-			else if (jmi.getName().equals("debug")) {
+			else if (jmi.getName().equals("teamView")) {
+				MainWindow.setDisplayBoardTeam();
+				JOptionPane.showMessageDialog(null, "SpyMaster Board View");
+			}
+			else if (jmi.getName().equals("spyView")) {
+				MainWindow.setDisplayBoardSpyMaster();
+				JOptionPane.showMessageDialog(null, "Team Turn Board View");
+			}
+			else if (jmi.getName().equals("cheatView")) {
+				//MainWindow.setDisplayCheatView;
+			}
+			else if (jmi.getName().equals("boardView")) {
 				if (MainWindow.get_currentBoardDisplayed().equals("team")) {
 					MainWindow.setDisplayBoardSpyMaster();
+					JOptionPane.showMessageDialog(null, "Team Turn Board View");
 				}
 				else if (MainWindow.get_currentBoardDisplayed().equals("spy")) {
 					MainWindow.setDisplayBoardTeam();
+					JOptionPane.showMessageDialog(null, "SpyMaster Board View");
 				}
 			}
-			
+			MainWindow.get_window().revalidate();
+			//MainWindow.get_window().setVisible(false);
+			//MainWindow.get_window().setVisible(true);
 		}
-		
+
 	}
 }
